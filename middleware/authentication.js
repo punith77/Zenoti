3d98e7d038893cc246ff483bc0d1fcc60e1bf188
Punith77 <punith.ch@gmail.com>
2020-04-15 20:44:44 -0600

    feat: Intial fead added CRUD functionality for city Weather data

---
+++ middleware/authentication.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const authenticationHandler = (req, res, next) => {
+    const headers = req.headers;
+
+    if (!headers.accesstoken) {
+        return res.status(403).json("Un-Authorized Access")
+    }
+    jwt.verify(headers.accesstoken, 'APP_SECRET', (err, result) => {
+        if (err) {
+            return res.status(403).json("Authentication Failed")
+        }
+        next();
+    })
+
+}
+
+module.exports = authenticationHandler
